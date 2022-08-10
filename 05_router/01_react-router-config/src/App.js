f38f25482115f223da991fc20080071c48b2a0fa
byeollim woo <byeollim.woo@gmail.com>
2022-08-10 16:18:38 +0900

    commit

--- 05_router/01_react-router-config/src/App.js
+++ 05_router/01_react-router-config/src/App.js
@@ -10,7 +10,10 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={ <Main/> }/>
+          {/* 루트 요청 시 Main으로 이동하도록 설정 */}
+          {/*<Route path="/" element={ <Main/> }/>*/}
+          {/* 인덱스로 설정해두면 위의 설정(루트 요청)과 동일 */}
+          <Route index element={ <Main/> }/>
           <Route path="/about" element={ <About/> }/>
           <Route path="/menu" element={ <Menu/> }/>
         </Routes>
