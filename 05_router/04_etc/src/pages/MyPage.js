ad650ef2b3bc43bfa36519318ba8011cf61e98f6
byeollim woo <byeollim.woo@gmail.com>
2022-08-11 15:23:07 +0900

    commit

--- 05_router/04_etc/src/pages/MyPage.js
+++ 05_router/04_etc/src/pages/MyPage.js
@@ -1,4 +1,12 @@
+import { Navigate } from 'react-router-dom';
+
 function MyPage() {
+
+    const isLogin = false;
+
+    if(!isLogin) {
+        return <Navigate to="/login" replace={ true }/>
+    }
 
     return (
         <h1>마이페이지</h1>
