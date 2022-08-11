1860738e6f6a71b5b09ceec7c0621f6d82d4e956
byeollim woo <byeollim.woo@gmail.com>
2022-08-11 11:59:56 +0900

    commit

--- 05_router/03_params/src/components/MenuItem.js
+++ 05_router/03_params/src/components/MenuItem.js
@@ -1,13 +1,17 @@
 import itemStyle from './MenuItem.module.css';
+import { Link } from 'react-router-dom';
 
 function MenuItem ({ menu }) {
 
+    /* path 형태로 메뉴코드 값을 전달한다.  */
     return (
-        <div className={ itemStyle.MenuItem }>
-            <h3>이름 : { menu.menuName }</h3>
-            <h3>가격 : { menu.menuPrice }</h3>
-            <h3>종류 : { menu.categoryName }</h3>
-        </div>
+        <Link to={`/menu/${ menu.menuCode }`}>
+            <div className={ itemStyle.MenuItem }>
+                <h3>이름 : { menu.menuName }</h3>
+                <h3>가격 : { menu.menuPrice }</h3>
+                <h3>종류 : { menu.categoryName }</h3>
+            </div>
+        </Link>
     );
 }
 
---
+++ 05_router/03_params/src/pages/MenuDetail.js
@@ -0,0 +1,6 @@
+
+function MenuDetail() {
+
+}
+
+export default MenuDetail;
