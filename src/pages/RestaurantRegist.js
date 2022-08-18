359e9657fab945f3ff47639b3b9e53f72f2f88a2
yuseungchul <yoosc14@gmail.com>
2022-08-19 02:48:13 +0900

    [유승철]RestaurantRegisForm 변경

--- src/pages/RestaurantRegist.js
+++ src/pages/RestaurantRegist.js
@@ -1,21 +1,20 @@
 import RestaurantRegistForm from '../components/form/RestaurantRegistForm';
-import './regist.css';
+import './RestaurantRegist.css';
 function RegistRestaurant() {
-
     return(
-        <>  
-        
-            <div className="container">
-                <div className="item">
-                <img className="regisimage" alt="regis_01" src="/images/images_main/register_picture.jpg"/>
+        <>
+            <div className="itembox">
+
+                <div className="item1">
+                    <img className="regisimage" alt="regis_01" src="/images/images_main/register_picture.jpg"/>
                 </div>
-        
-                <div className="item">
+
+                <div className="item1">
                     <h1>메뉴 등록 페이지</h1>
                     <RestaurantRegistForm/>
                 </div>
 
-                <div className="item">
+                <div className="item1">
                     <div className="bottomword">
                         <h1 className="bottomtitle">강남뭐먹지?!</h1>
                         <p><b>02-1234-5678</b><br/>
@@ -23,11 +22,16 @@ function RegistRestaurant() {
                            <b>gangnam@mysite.com</b></p>
                     </div>
                 </div>
+
+                <div className="introduce">
+                       <h2 >Introduce you <br/>
+                            to Restaurant <br/>
+                               in GangNam</h2>
+                    
+                </div>  
                 
-               
             </div>
         </>
     );
 }
-
 export default RegistRestaurant;
